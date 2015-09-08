cd7bcb8b3740d64639105ea57f60c739bc90b6ae
hangzhou-huxin <huxinstudy@163.com>
2015-09-08 18:39:46 +0800

    修改第四步的题的选项值

--- study/src/main/webapp/js/holland/step4.js
+++ study/src/main/webapp/js/holland/step4.js
@@ -1,26 +1,26 @@
 
-questions1 = [{qid:"h_r4_1",index:"1.",context:"如果工作中能经常用到我特别的技巧和才能，我会感到特别满意。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]},
-                  {qid:"h_r4_2",index:"2.",context:"对我而言，做一个专业领域的部门经理比做总经理更具有吸引力。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]}
+questions1 = [{qid:"h_r4_1",index:"1.",context:"如果工作中能经常用到我特别的技巧和才能，我会感到特别满意。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]},
+                  {qid:"h_r4_2",index:"2.",context:"对我而言，做一个专业领域的部门经理比做总经理更具有吸引力。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]}
                    ] ;
 
-questions2 = [{qid:"h_e4_1",index:"1.",context:"我追求需要计划和组织别人的工作。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]},
-              {qid:"h_e4_2",index:"2.",context:"我希望能够管理一个大的公司（组织），我的决策将会影响许多人",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]}
+questions2 = [{qid:"h_e4_1",index:"1.",context:"我追求需要计划和组织别人的工作。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]},
+              {qid:"h_e4_2",index:"2.",context:"我希望能够管理一个大的公司（组织），我的决策将会影响许多人",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]}
               ];
 
-questions3 = [{qid:"h_c4_1",index:"1.",context:"我愿意在能给我安全感和稳定感的公司中工作。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]},
-              {qid:"h_c4_2",index:"2.",context:"我希望我的职业允许兼顾到个人、家庭和工作的需要。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]}
+questions3 = [{qid:"h_c4_1",index:"1.",context:"我愿意在能给我安全感和稳定感的公司中工作。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]},
+              {qid:"h_c4_2",index:"2.",context:"我希望我的职业允许兼顾到个人、家庭和工作的需要。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]}
              ];
 
-questions4 = [{qid:"h_s4_1",index:"1.",context:"由于我的工作，经常有许多人来感谢我，让我感到满足。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]},
-              {qid:"h_s4_2",index:"2.",context:"在工作中，我为他人服务，使他人感到很满意，我自己也很高兴。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]}
+questions4 = [{qid:"h_s4_1",index:"1.",context:"由于我的工作，经常有许多人来感谢我，让我感到满足。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]},
+              {qid:"h_s4_2",index:"2.",context:"在工作中，我为他人服务，使他人感到很满意，我自己也很高兴。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]}
              ];
 
-questions5 = [{qid:"h_i4_1",index:"1.",context:"去解决那些几乎无法解决的难题比获得一个高的管理职位更重要。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]},
-              {qid:"h_i4_2",index:"2.",context:"我认为职业的成功来自于克服自己面临的非常困难的挑战。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]}
+questions5 = [{qid:"h_i4_1",index:"1.",context:"去解决那些几乎无法解决的难题比获得一个高的管理职位更重要。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]},
+              {qid:"h_i4_2",index:"2.",context:"我认为职业的成功来自于克服自己面临的非常困难的挑战。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]}
               ];
 
-questions6 = [{qid:"h_a4_1",index:"1.",context:"如果我能成功地实现完全属于自己的产品（点子），我会感到非常成功。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]},
-              {qid:"h_a4_2",index:"2.",context:"在我的工作中，不会有人常来打扰我。",options:[{text:"不符",value:"true"},{text:"较不符",value:"false"},{text:"一般",value:"false"},{text:"较符合",value:"false"},{text:"符合",value:"false"}]}
+questions6 = [{qid:"h_a4_1",index:"1.",context:"如果我能成功地实现完全属于自己的产品（点子），我会感到非常成功。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]},
+              {qid:"h_a4_2",index:"2.",context:"在我的工作中，不会有人常来打扰我。",options:[{text:"不符",value:"a"},{text:"较不符",value:"b"},{text:"一般",value:"c"},{text:"较符合",value:"d"},{text:"符合",value:"e"}]}
                ];
 
 
